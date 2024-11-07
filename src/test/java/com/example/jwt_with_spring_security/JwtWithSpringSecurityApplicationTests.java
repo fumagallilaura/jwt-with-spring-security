f774b580b1054ef02e5cea3589d27a292d743a20
Laura Fumagalli <la_12_msn@hotmail.com>
2024-11-07 16:48:57 -0300

    test: add test config JwtWithSpringSecurityApplication.class

--- src/test/java/com/example/jwt_with_spring_security/JwtWithSpringSecurityApplicationTests.java
+++ src/test/java/com/example/jwt_with_spring_security/JwtWithSpringSecurityApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.jwt_with_spring_security;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JwtWithSpringSecurityApplication.class)
 class JwtWithSpringSecurityApplicationTests {
 
 	@Test
